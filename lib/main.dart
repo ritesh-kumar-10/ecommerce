d51100c8c1a6abc187fbefecb5a69cb28f9836ff
Ritesh <riteshkumarsahu64@gmail.com>
2023-11-24 22:28:08 +0530

    adding files

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce/constants/Global_variables.dart';
+import 'package:ecommerce/features/auth/screen/auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce/router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            )),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+    );
+  }
+}
